6c0273ed07a1a73f840c55a3faffd76de420cecf
RICHARD HENRIQUE <richardhenriquedeabreuferreira@gmail.com>
2026-02-14 00:31:45 -0300

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: 'class', // Essencial para ativar via classe no HTML
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      // Suas pecinhas de Lego personalizadas
+      fundo: 'var(--cor-fundo)',
+      card: 'var(--cor-card)',
+      borda: 'var(--cor-borda)',
+      'texto-base': 'var(--cor-texto-principal)',
+      'texto-sub': 'var(--cor-texto-secundario)',
+      primaria: 'var(--cor-primaria)',
+      'primaria-hover': 'var(--cor-primaria-hover)',
+      sucesso: 'var(--cor-sucesso)',
+      erro: 'var(--cor-erro)',
+      branco: '#FFFFFF', // utilitário fixo
+    },
   },
   plugins: [],
 }
